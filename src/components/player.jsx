9db4fa60225104d4109862d31c8ae5d4ddb275a9
Yana Hlazkova <yglazkova8@gmail.com>
2024-01-28 22:07:13 +0200

    add click on button navicon

---
+++ src/components/player.jsx
@@ -0,0 +1,5 @@
+const navicon = document.querySelector(".fa.fa-bars");
+navicon.addEventListener("click", () => alert("sfasfs"));
+export default function Player() {
+  return <div> Ваш компонент Player</div>;
+}
